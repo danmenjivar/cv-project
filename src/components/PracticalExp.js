f4922bb084ac862eef560e102d9d3e0c58d0cb17
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-06-29 11:55:23 -0700

    Add PracticalExperince Component

---
+++ src/components/PracticalExp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PracticalExpElement from "./PracticalExpElement";
+
+export default function PracticalExp() {
+  return (
+    <div>
+      <h3>Practical Experience</h3>
+      <PracticalExpElement />
+    </div>
+  );
+}
---
+++ src/components/PracticalExpElement.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function PracticalExpElement() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="companyName">
+          <input
+            type="text"
+            name="companyName"
+            id="companyName"
+            placeholder="Company Name"
+          />
+        </label>
+        <label htmlFor="positionTitle">
+          <input
+            type="text"
+            name="positionTitle"
+            id="positionTitle"
+            placeholder="Position Title"
+          />
+        </label>
+        <label htmlFor="dateFrom">
+          <input type="text" name="dateFrom" id="dateFrom" placeholder="From" />
+        </label>
+        <label htmlFor="dateUntil">
+          <input type="text" name="dateUntil" id="dateUntil" placeholder="To" />
+        </label>
+        <label htmlFor="city">
+          <input type="text" name="city" id="city" placeholder="City" />
+        </label>
+        <label htmlFor="state">
+          <input type="text" name="state" id="state" placeholder="State" />
+        </label>
+        {/* TODO: add switch for remote work */}
+        <label htmlFor="responsibilities">
+          <textarea
+            type="text"
+            name="responsibilities"
+            id="responsibilities"
+            placeholder="Main activities and responsibilities"
+          />
+        </label>
+      </form>
+      <button>Add</button>
+    </div>
+  );
+}
