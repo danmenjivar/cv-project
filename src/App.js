42b34326e3d0092f31fceb7ee2168b402e26883f
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-06-29 10:56:22 -0700

    Add first NavBar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import Nav from "./components/Nav";
+import PersonalInfo from "./components/PersonalInfo";
 
+// Note: color palette https://coolors.co/ee6c4d-f38d68-662c91-17a398-33312e
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
