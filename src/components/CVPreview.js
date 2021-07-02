74be367866753ac85d2ba3ec3aed0ad785ca9df7
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-07-02 13:10:47 -0700

    Add formatting to CV

--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -13,8 +13,13 @@ export default class CVPreview extends Component {
         <h3>
           {personalInfo.firstName} {personalInfo.lastName}
         </h3>
+        <div className="socials">
+          <Linkedin />@{personalInfo.linkedIn}
+          {" \u2022 "}
+          <Github />@{personalInfo.github}
+        </div>
         <hr></hr>
-        <p>
+        <div className="personal">
           {personalInfo.streetAddress}
           {" \u2022 "}
           {personalInfo.city},{personalInfo.state} {personalInfo.zipCode}
@@ -22,46 +27,74 @@ export default class CVPreview extends Component {
           {personalInfo.email}
           {" \u2022 "}
           {personalInfo.phone}
-          {" \u2022 "}
-          <Linkedin />@{personalInfo.linkedIn}
-          {" \u2022 "}
-          <Github />@{personalInfo.github}
-        </p>
+        </div>
         <h2>Education</h2>
         {edInfo.map((ed) => {
           return (
-            <p>
-              {ed.schoolName} {ed.degreeOfStudy} {ed.gradDate} {ed.city}{" "}
-              {ed.state} {ed.relStudy}
-            </p>
+            <div className="grid">
+              <div className="row1-col1">{ed.schoolName}</div>
+              <div className="row1-col2">
+                {ed.city}, {ed.state}
+              </div>
+              <div className="row2-col1">{ed.degreeOfStudy}</div>
+              <div className="row2-col2">{ed.gradDate}</div>
+              <div className="row3">{ed.relStudy}</div>
+            </div>
           );
         })}
         <h2>Employment</h2>
         {expInfo.map((exp) => {
           return (
-            <p>
-              {exp.companyName} {exp.positionTitle} {exp.startDate}{" "}
-              {exp.endDate} {exp.city} {exp.state} {exp.resp}
-            </p>
+            <div className="grid">
+              <div className="row1-col1">{exp.companyName}</div>
+              <div className="row1-col2">
+                {exp.city}, {exp.state}
+              </div>
+              <div className="row2-col1">{exp.positionTitle}</div>
+              <div className="row2-col2">
+                {exp.startDate}-{exp.endDate}
+              </div>
+              <div className="row3">{exp.resp}</div>
+            </div>
           );
         })}
         <h2>Technical Experience</h2>
+        {techInfo.projs.length > 0 && <h3>Projects</h3>}
         {techInfo.projs.map((proj) => {
           return (
-            <p>
-              {proj.title}
-              {proj.year}
-              {proj.desc}
-              {proj.etc}
-            </p>
+            <div className="justify-left">
+              {" \u2022 "}
+              <span className="elemTitle">{proj.title}</span> ({proj.year}).{" "}
+              {proj.desc}. {proj.etc}.
+            </div>
           );
         })}
-        {techInfo.langs.map((lang) => {
-          return <p>{lang.name}</p>;
-        })}
-        {techInfo.tools.map((tech) => {
-          return <p>{tech.name}</p>;
-        })}
+        <div className="justify-left">
+          {techInfo.langs.length > 0 && (
+            <span className="elemTitle">Languages: </span>
+          )}
+          {techInfo.langs.map((lang, i) => {
+            return (
+              <span>
+                {lang.name}
+                {techInfo.langs.length === i + 1 ? "." : ", "}
+              </span>
+            );
+          })}
+        </div>
+        <div className="justify-left">
+          {techInfo.tools.length > 0 && (
+            <span className="elemTitle">Technologies: </span>
+          )}
+          {techInfo.tools.map((tech, i) => {
+            return (
+              <span>
+                {tech.name}
+                {techInfo.tools.length === i + 1 ? "." : ", "}
+              </span>
+            );
+          })}
+        </div>
       </div>
     );
   }
